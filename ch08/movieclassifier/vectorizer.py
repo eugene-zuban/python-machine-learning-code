81db8e07551d435d15eb2c18a8b13ec388203ea7
Jack Zuban <jack.zuban@gmail.com>
2017-06-25 13:14:00 -0500

    add space

--- ch08/movieclassifier/vectorizer.py
+++ ch08/movieclassifier/vectorizer.py
@@ -9,7 +9,7 @@ stop = pickle.load(open(os.path.join(cur_dir, 'pkl_objects', 'stopwords.pkl'), '
 def tokenizer(text):
     text = re.sub('<[^>]*>', '', text)
     emoticons = re.findall('(?::|;|=)(?:-)?(?:\)|\(|D|P)', text.lower())
-    text = re.sub('[\W]+', ' ', text.lower()) + ''.join(emoticons).replace('-', '')
+    text = re.sub('[\W]+', ' ', text.lower()) + ' '.join(emoticons).replace('-', '')
     tokenized = [w for w in text.split() if w not in stop]
     return tokenized
 
